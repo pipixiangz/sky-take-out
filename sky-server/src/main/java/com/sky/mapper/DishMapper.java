7d961a732bb8ab46f7daf6f41d7ab9c971154892
pipixiangz <763038134@qq.com>
2024-02-22 16:05:21 +0800

    新增套餐

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -66,4 +66,10 @@ public interface DishMapper {
     @AutoFill(value = OperationType.UPDATE)
     void update(Dish dish);
 
+    /**
+     * 动态条件查询菜品
+     * @param dish
+     * @return
+     */
+    List<Dish> list(Dish dish);
 }
--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -165,4 +165,10 @@ public class DishServiceImpl implements DishService {
         Dish dish = Dish.builder().status(status).id(id).build();
         dishMapper.update(dish);
     }
+
+    @Override
+    public List<Dish> list(Long categoryId) {
+        Dish dish = Dish.builder().categoryId(categoryId).status(StatusConstant.ENABLE).build();
+        return dishMapper.list(dish);
+    }
 }
